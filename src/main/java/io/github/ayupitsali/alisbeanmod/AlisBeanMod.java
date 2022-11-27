5f2b8b6d684f6e78836d652efa3568266f40dbc4
Ali <ali.dee1410@gmail.com>
2022-11-27 13:04:09 +0000

    Initial commit

---
+++ src/main/java/io/github/ayupitsali/alisbeanmod/AlisBeanMod.java
@@ -0,0 +1,38 @@
+package io.github.ayupitsali.alisbeanmod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(AlisBeanMod.MOD_ID)
+public class AlisBeanMod {
+    public static final String MOD_ID = "alisbeanmod";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public AlisBeanMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("IT'S BEAN TIME!");
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
