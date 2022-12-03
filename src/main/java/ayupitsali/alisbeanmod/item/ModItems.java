f944fffc0647a896e32d5a3d0d2d6330fa8293ec
Ali <ali.dee1410@gmail.com>
2022-12-03 16:38:36 +0000

    Ali's Bean Mod creative mode tab

--- src/main/java/ayupitsali/alisbeanmod/item/ModItems.java
+++ src/main/java/ayupitsali/alisbeanmod/item/ModItems.java
@@ -2,7 +2,6 @@ package ayupitsali.alisbeanmod.item;
 
 import ayupitsali.alisbeanmod.AlisBeanMod;
 import com.epherical.croptopia.util.FoodConstructor;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,13 +12,21 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, AlisBeanMod.MOD_ID);
 
     public static final RegistryObject<Item> BLACK_EYED_BEANS = ITEMS.register("black_eyed_beans",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(FoodConstructor.createFood(FoodConstructor.REG_2))));
+            () -> new Item(new Item.Properties()
+                    .tab(ModItemGroups.ALIS_BEAN_MOD)
+                    .food(FoodConstructor.createFood(FoodConstructor.REG_2))));
     public static final RegistryObject<Item> BROAD_BEANS = ITEMS.register("broad_beans",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(FoodConstructor.createFood(FoodConstructor.REG_3))));
+            () -> new Item(new Item.Properties()
+                    .tab(ModItemGroups.ALIS_BEAN_MOD)
+                    .food(FoodConstructor.createFood(FoodConstructor.REG_3))));
     public static final RegistryObject<Item> HARICOT_BEANS = ITEMS.register("haricot_beans",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(FoodConstructor.createFood(FoodConstructor.REG_4))));
+            () -> new Item(new Item.Properties()
+                    .tab(ModItemGroups.ALIS_BEAN_MOD)
+                    .food(FoodConstructor.createFood(FoodConstructor.REG_4))));
     public static final RegistryObject<Item> KIDNEY_BEANS = ITEMS.register("kidney_beans",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).food(FoodConstructor.createFood(FoodConstructor.REG_3))));
+            () -> new Item(new Item.Properties()
+                    .tab(ModItemGroups.ALIS_BEAN_MOD)
+                    .food(FoodConstructor.createFood(FoodConstructor.REG_3))));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
